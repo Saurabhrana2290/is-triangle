b75eaaf9b29e3576a80de7ac1d6a404d9bf03ee1
Saurabhrana2290 <saurabhrana532@gmail.com>
2021-09-20 22:03:43 +0530

    Create- hypotenuse logic created

---
+++ hypotenuse.js
@@ -0,0 +1,16 @@
+const sides = document.querySelectorAll(".sides");
+const calculateHypotenuseButton = document.querySelector("#calculate-hypotenuse-btn");
+const output = document.querySelector("#output");
+
+function sumOfSquaresOfSides(a,b){
+    const sum = a*a + b*b;
+    return sum;
+}
+
+function calculateHypotenuse(){
+    const sumOfSquares = sumOfSquaresOfSides(Number(sides[0].value),Number(sides[1].value));
+    const hypotenuseLength = Math.sqrt(sumOfSquares);
+    output.innerText = hypotenuseLength;
+}
+
+calculateHypotenuseButton.addEventListener("click",calculateHypotenuse);
